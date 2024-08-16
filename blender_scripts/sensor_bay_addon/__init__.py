7046b06c127d9ba2cab0c676067902c5af4ac3d7
Carson Kohlbrenner <82614880+cKohl10@users.noreply.github.com>
2024-08-16 00:01:55 -0600

    Updated Poission densities

--- blender_scripts/sensor_bay_addon/__init__.py
+++ blender_scripts/sensor_bay_addon/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Tactile Sensor Bay",
     "author": "Carson Kohlbrenner",
-    "version": (1, 6),
+    "version": (1, 8),
     "blender": (2, 80, 0),
     "location": "Object > Sensor Bay",
     "description": "Paint on tactile sensors over a surface and save them for Isaac Sim",
@@ -16,6 +16,15 @@ import bpy.props
 from .operators.isaac_save_operator import IsaacSaveOperator
 from .operators.alligator_save_operator import AlligatorSaveOperator
 
+class MyAddonProperties(bpy.types.PropertyGroup):
+    unit_scale: bpy.props.FloatProperty(
+        name="Unit Scale",
+        description="Scale of the units",
+        default=1.0,
+        min=0.001,
+        max=1000000.0
+    )
+
 class SensorPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
     bl_label = "Sensor Bay"
@@ -26,7 +35,8 @@ class SensorPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-
+        scene = context.scene
+        unit_prop = scene.my_addon_properties
         obj = context.object
 
         row = layout.row()
@@ -36,6 +46,11 @@ class SensorPanel(bpy.types.Panel):
         row.label(text="Selected root prim: " + obj.name)
         row = layout.row()
         row.prop(obj, "name")
+
+        # Unit Scale Slider
+        row = layout.row()
+        row.label(text="Unit Scale")
+        row.prop(unit_prop, "unit_scale")
 
         # Isaac Save Button 
         row = layout.row()
@@ -51,6 +66,8 @@ class SensorPanel(bpy.types.Panel):
 
 
 def register():
+    bpy.utils.register_class(MyAddonProperties)
+    bpy.types.Scene.my_addon_properties = bpy.props.PointerProperty(type=MyAddonProperties)
     bpy.utils.register_class(IsaacSaveOperator)
     bpy.utils.register_class(AlligatorSaveOperator)
     bpy.utils.register_class(SensorPanel)
@@ -58,6 +75,8 @@ def register():
 def unregister():
     bpy.utils.unregister_class(IsaacSaveOperator)
     bpy.utils.unregister_class(AlligatorSaveOperator)
+    bpy.utils.unregister_class(MyAddonProperties)
+    del bpy.types.Scene.my_addon_properties
     bpy.utils.unregister_class(SensorPanel)
     
 
--- blender_scripts/sensor_bay_addon/operators/alligator_save_operator.py
+++ blender_scripts/sensor_bay_addon/operators/alligator_save_operator.py
@@ -27,9 +27,10 @@ class AlligatorSaveOperator(Operator, ExportHelper):
     )
     
     def execute(self, context):
+        self.unit_scale = context.scene.my_addon_properties.unit_scale
         print("AlligatorSaveOperator.execute called\n")
         if self.filepath:  # Check if filepath has been set
-            save_attribute_to_csv(context, self.filepath)
+            self.save_attribute_to_csv(context, self.filepath)  # Save the sensor positions to a CSV file
         else:
             self.report({'WARNING'}, "No file selected")  # Report a warning if no file was selected
             return {'CANCELLED'}
@@ -40,216 +41,217 @@ class AlligatorSaveOperator(Operator, ExportHelper):
         return {'RUNNING_MODAL'}
     
     # This function recursively checks the children of the object for sensors
-def check_children_for_sensors(obj, parent_path, folder_path):
+    def check_children_for_sensors(self, obj, parent_path, folder_path):
 
-    sensor_data = {}
-    sensor_data_all = []
+        sensor_data = {}
+        sensor_data_all = []
 
-    # Get the parent path from the root object
-    parent_path = parent_path + "/" + obj.name
+        # Get the parent path from the root object
+        parent_path = parent_path + "/" + obj.name
 
-    is_sensor_attribute_name = "is_sensor"
-    pos_attribute_name = "position"
-    rad_attribute_name = "radii"
-    alligator_clip_attribute_name = "is_clip"
-    default_radius = False
+        is_sensor_attribute_name = "is_sensor"
+        pos_attribute_name = "position"
+        rad_attribute_name = "radii"
+        alligator_clip_attribute_name = "is_clip"
+        default_radius = False
 
-    # Loop through all of the children objects and search for GeometryNodes modifier
-    for child in obj.children:
-        sensor_data[child.name] = False
-        pos_attribute_data = []
+        # Loop through all of the children objects and search for GeometryNodes modifier
+        for child in obj.children:
+            sensor_data[child.name] = False
+            pos_attribute_data = []
 
-        # Recursively check the children for sensors
-        child_sensor_data = check_children_for_sensors(child, parent_path, folder_path)
+            # Recursively check the children for sensors
+            child_sensor_data = self.check_children_for_sensors(child, parent_path, folder_path)
 
-        # Ensure the object has geometry nodes modifier
-        if child.modifiers.get('Skin') is None:
-            # print(f"{child.name} does not have a Skin modifier.")
+            # Ensure the object has geometry nodes modifier
+            if child.modifiers.get('Skin') is None:
+                # print(f"{child.name} does not have a Skin modifier.")
+                # Add the child sensor data to the sensor data list
+                sensor_data[child.name] = child_sensor_data
+                continue
+
+            #print(f"Found Skin modifier in object {child.name}.")
+
+            # Export the object with skin modifier as an obj file
+            self.export_object(child, folder_path + "/" + child.name + ".stl")
+
+            # Get the evaluated geometry
+            depsgraph = bpy.context.evaluated_depsgraph_get()
+            eval_obj = child.evaluated_get(depsgraph)
+            mesh = eval_obj.to_mesh()
+            
+            # Check if the position data exists
+            if pos_attribute_name not in mesh.attributes:
+                #print(f"Attribute {attribute_name} not found in object {child.name}.")
+                # for other_name in mesh.attributes:
+                #     print(f"Found attribute: {other_name}.")
+                # Add the child sensor data to the sensor data list
+                sensor_data[child.name] = child_sensor_data
+                continue
+
+            if is_sensor_attribute_name not in mesh.attributes:
+                #print(f"Attribute {is_sensor_attribtue_name} not found in object {child.name}.")
+                # Add the child sensor data to the sensor data list
+                sensor_data[child.name] = child_sensor_data
+                continue
+
+            if rad_attribute_name not in mesh.attributes:
+                #Set a default radius value if the radii attribute is not found
+                print(f"Attribute {rad_attribute_name} not found in object {child.name}. Setting default radius of 0.1.")
+                default_radius = True
+        
+            # Get the attribute data
+
+            pos_attribute_data = mesh.attributes[pos_attribute_name].data
+
+            # Get the radii attribute data
+            rad_attribute_data = []
+            if not default_radius:
+                rad_attribute_data = mesh.attributes[rad_attribute_name].data
+
+            is_sensor_data = mesh.attributes[is_sensor_attribute_name].data
+
+            # Get path to object
+            parent_path = parent_path + "/" + child.name
+            # Remove any triple digit numbers from the parent path
+            parent_path = re.sub(r'\.\d{3,}', '', parent_path)
+
+            # Check if the alligator clip attribute exists
+            clip_pos = []
+            if alligator_clip_attribute_name in mesh.attributes:
+                is_clip_data = mesh.attributes[alligator_clip_attribute_name].data
+                # Get the position data of the alligator clip
+                for i in range(len(is_clip_data)):
+                    if is_clip_data[i].value:
+                        clip_pos.append(SensorData(mesh.attributes[pos_attribute_name].data[i].vector, 0.1, parent_path, True, name=child.name))
+            
+                print("\n\n ############################################## \n\n")
+                for clip in clip_pos:
+                    print(f"    Alligator clip position found at {clip.pos} in object {child.name}.")
+                print("\n\n ############################################## \n\n")
+
+            # Add the alligator clip data to the sensor data list as the first element
+            # The alligator clips will sandwich the sensor data to complete the node loop
+            if len(clip_pos) > 0:
+                child_sensor_data.append(clip_pos[0])
+            
+            # Add the attribute data to the sensor data list
+            sensor_counter = 0
+            for i in range(len(pos_attribute_data)):
+                if is_sensor_data[i].value:
+                    if default_radius:
+                        child_sensor_data.append(SensorData(pos_attribute_data[i].vector, 0.1, parent_path, name=child.name))
+                    else:
+                        child_sensor_data.append(SensorData(pos_attribute_data[i].vector, rad_attribute_data[i].value, parent_path, name=child.name))
+
+                    sensor_counter = sensor_counter + 1
+
+            #print(f"Found {sensor_counter} sensor positions in object {child.name} under {parent_path}.")
+
+            # Clean up
+            #eval_obj.to_mesh_clear()
+
+            # Add the second alligator clip data to the sensor data list as the last element is it exists
+            if len(clip_pos) > 1:
+                child_sensor_data.append(clip_pos[1])
+
             # Add the child sensor data to the sensor data list
             sensor_data[child.name] = child_sensor_data
-            continue
 
-        #print(f"Found Skin modifier in object {child.name}.")
-
-        # Export the object with skin modifier as an obj file
-        export_object(child, folder_path + "/" + child.name + ".obj")
-
-        # Get the evaluated geometry
-        depsgraph = bpy.context.evaluated_depsgraph_get()
-        eval_obj = child.evaluated_get(depsgraph)
-        mesh = eval_obj.to_mesh()
-        
-        # Check if the position data exists
-        if pos_attribute_name not in mesh.attributes:
-            #print(f"Attribute {attribute_name} not found in object {child.name}.")
-            # for other_name in mesh.attributes:
-            #     print(f"Found attribute: {other_name}.")
-            # Add the child sensor data to the sensor data list
-            sensor_data[child.name] = child_sensor_data
-            continue
-
-        if is_sensor_attribute_name not in mesh.attributes:
-            #print(f"Attribute {is_sensor_attribtue_name} not found in object {child.name}.")
-            # Add the child sensor data to the sensor data list
-            sensor_data[child.name] = child_sensor_data
-            continue
-
-        if rad_attribute_name not in mesh.attributes:
-            #Set a default radius value if the radii attribute is not found
-            print(f"Attribute {rad_attribute_name} not found in object {child.name}. Setting default radius of 0.1.")
-            default_radius = True
-    
-        # Get the attribute data
-
-        pos_attribute_data = mesh.attributes[pos_attribute_name].data
-
-        # Get the radii attribute data
-        rad_attribute_data = []
-        if not default_radius:
-            rad_attribute_data = mesh.attributes[rad_attribute_name].data
-
-        is_sensor_data = mesh.attributes[is_sensor_attribute_name].data
-
-        # Get path to object
-        parent_path = parent_path + "/" + child.name
-        # Remove any triple digit numbers from the parent path
-        parent_path = re.sub(r'\.\d{3,}', '', parent_path)
-
-        # Check if the alligator clip attribute exists
-        clip_pos = []
-        if alligator_clip_attribute_name in mesh.attributes:
-            is_clip_data = mesh.attributes[alligator_clip_attribute_name].data
-            # Get the position data of the alligator clip
-            for i in range(len(is_clip_data)):
-                if is_clip_data[i].value:
-                    clip_pos.append(SensorData(mesh.attributes[pos_attribute_name].data[i].vector, 0.1, parent_path, True))
-        
-            print("\n\n ############################################## \n\n")
-            for clip in clip_pos:
-                print(f"    Alligator clip position found at {clip.pos} in object {child.name}.")
-            print("\n\n ############################################## \n\n")
-
-        # Add the alligator clip data to the sensor data list as the first element
-        # The alligator clips will sandwich the sensor data to complete the node loop
-        if len(clip_pos) > 0:
-            child_sensor_data.append(clip_pos[0])
-        
-        # Add the attribute data to the sensor data list
-        sensor_counter = 0
-        for i in range(len(pos_attribute_data)):
-            if is_sensor_data[i].value:
-                if default_radius:
-                    child_sensor_data.append(SensorData(child.name, pos_attribute_data[i].vector, 0.1, parent_path))
-                else:
-                    child_sensor_data.append(SensorData(child.name, pos_attribute_data[i].vector, rad_attribute_data[i].value, parent_path))
-
-                sensor_counter = sensor_counter + 1
-
-        #print(f"Found {sensor_counter} sensor positions in object {child.name} under {parent_path}.")
-
-        # Clean up
-        #eval_obj.to_mesh_clear()
-
-        # Add the second alligator clip data to the sensor data list as the last element is it exists
-        if len(clip_pos) > 1:
-            child_sensor_data.append(clip_pos[1])
-
-        # Add the child sensor data to the sensor data list
-        sensor_data[child.name] = child_sensor_data
-
-    # Print the sensor data
-    if len(obj.children) == 0:
-        return sensor_data_all
-    else:
-        total_sensor_count = 0
-        print(f"\nObject {obj.name} has {len(obj.children)} child(ren):")
-        for child in obj.children: # Loop through all of the children objects
-            # Identify how many of the positions in the child are sensors and not clips
-            num_sensors = 0
-            for sensor in sensor_data[child.name]:
-                if sensor.is_clip == False:
-                    num_sensors = num_sensors + 1
-
-            if num_sensors == 0:
-                print(f"        Child: {child.name} has no sensors.")
-            else:
-                print(f"        Child: {child.name} has {num_sensors} sensors.")
-                total_sensor_count = total_sensor_count + num_sensors
-
+        # Print the sensor data
+        if len(obj.children) == 0:
+            return sensor_data_all
+        else:
+            total_sensor_count = 0
+            print(f"\nObject {obj.name} has {len(obj.children)} child(ren):")
+            for child in obj.children: # Loop through all of the children objects
+                # Identify how many of the positions in the child are sensors and not clips
+                num_sensors = 0
                 for sensor in sensor_data[child.name]:
-                    sensor_data_all.append(sensor)
+                    if sensor.is_clip == False:
+                        num_sensors = num_sensors + 1
 
-        print(f"Returning {total_sensor_count} sensor positions in object {obj.name} under {parent_path}.")
-    return sensor_data_all
+                if num_sensors == 0:
+                    print(f"        Child: {child.name} has no sensors.")
+                else:
+                    print(f"        Child: {child.name} has {num_sensors} sensors.")
+                    total_sensor_count = total_sensor_count + num_sensors
 
-# This function saves the sensor positions to a CSV file
-def save_attribute_to_csv(context, folder_path):
-    # Get the object
-    obj = context.object
+                    for sensor in sensor_data[child.name]:
+                        sensor_data_all.append(sensor)
 
-    # Expand the ~ symbol into the path of the home directory
-    #file_path = os.path.expanduser(file_path)
+            print(f"Returning {total_sensor_count} sensor positions in object {obj.name} under {parent_path}.")
+        return sensor_data_all
 
-    # Create a folder at the folder_path to save all exported files 
-    mesh_folder_path = folder_path + '/meshes'
-    os.makedirs(folder_path, exist_ok=True)
-    os.makedirs(mesh_folder_path, exist_ok=True)
+    # This function saves the sensor positions to a CSV file
+    def save_attribute_to_csv(self, context, folder_path):
+        # Get the object
+        obj = context.object
 
-    # Make an array of all sensor positions,radii, and parent paths
-    sensor_data = []
-    
-    # Check the children for sensors
-    sensor_data = check_children_for_sensors(obj, "", mesh_folder_path)
+        # Expand the ~ symbol into the path of the home directory
+        #file_path = os.path.expanduser(file_path)
 
-    # Check if there are any sensor positions
-    if len(sensor_data) == 0:
-        print("No sensor positions found.")
-        return
+        # Create a folder at the folder_path to save all exported files 
+        mesh_folder_path = folder_path + '/meshes'
+        os.makedirs(folder_path, exist_ok=True)
+        os.makedirs(mesh_folder_path, exist_ok=True)
 
-    # Save the attribute data to CSV
-    index_counter = 1
-    with open(folder_path + '/sensor_config.csv', 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['Index', 'X', 'Y', 'Z', 'Radius', 'Parent'])
+        # Make an array of all sensor positions,radii, and parent paths
+        sensor_data = []
         
-        for element in sensor_data:
-            if element.name != curr_name:
-                curr_name = element.name
-                csv_writer.writerow(["element.name"])
-                csv_writer.writerow([""])
+        # Check the children for sensors
+        sensor_data = self.check_children_for_sensors(obj, "", mesh_folder_path)
 
-            pos = element.pos
+        # Check if there are any sensor positions
+        if len(sensor_data) == 0:
+            print("No sensor positions found.")
+            return
 
-            # Check if the sensor is an alligator clip
-            if element.is_clip:
-                csv_writer.writerow(["CLIP", pos.x, pos.y, pos.z, element.radius, element.parent])
-            else:
-                csv_writer.writerow([index_counter, pos.x, pos.y, pos.z, element.radius, element.parent])
-                index_counter = index_counter + 1
-    
-    # print(f"\nAttribute {attribute_name} saved to {file_path}")
-    print(f"Sensor count: {len(sensor_data)}")
+        # Save the attribute data to CSV
+        index_counter = 1
+        curr_name = ""
+        with open(folder_path + '/sensor_config.csv', 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(['Index', 'X', 'Y', 'Z', 'Radius', 'Parent'])
+            
+            for element in sensor_data:
+                if element.name != curr_name:
+                    curr_name = element.name
+                    csv_writer.writerow(["", "", "", "", "", ""])
+                    csv_writer.writerow([element.name, "", "", "", "", ""])
 
-# Saves the selected sensors as an STL file
-def export_object(ob, file_path):
+                pos = element.pos
 
-    # Deselect all objects
-    bpy.ops.object.select_all(action='DESELECT')
+                # Check if the sensor is an alligator clip
+                if element.is_clip:
+                    csv_writer.writerow(["CLIP", pos.x * self.unit_scale, pos.y * self.unit_scale, pos.z * self.unit_scale, element.radius, element.parent])
+                else:
+                    csv_writer.writerow([index_counter, pos.x * self.unit_scale, pos.y * self.unit_scale, pos.z * self.unit_scale, element.radius, element.parent])
+                    index_counter = index_counter + 1
+        
+        # print(f"\nAttribute {attribute_name} saved to {file_path}")
+        print(f"Sensor count: {len(sensor_data)}")
 
-    # Set the selected object to the active object
-    ob.select_set(True)
-    bpy.context.view_layer.objects.active = ob
+    # Saves the selected sensors as an STL file
+    def export_object(self, ob, file_path):
 
-    #bpy.ops.export_mesh.stl(filepath=file_path, use_selection=True)
-    bpy.ops.wm.stl_export(filepath=file_path, export_selected_objects=True, use_scene_unit=True)
-    
-############################################################
-##################### Helper Functions #####################
-############################################################
+        # Deselect all objects
+        bpy.ops.object.select_all(action='DESELECT')
+
+        # Set the selected object to the active object
+        ob.select_set(True)
+        bpy.context.view_layer.objects.active = ob
+
+        #bpy.ops.export_mesh.stl(filepath=file_path, use_selection=True)
+        bpy.ops.wm.stl_export(filepath=file_path, export_selected_objects=True, global_scale=self.unit_scale)
+        
+    ############################################################
+    ##################### Helper Functions #####################
+    ############################################################
 
 # This sensor data class is used to store infomation about each sensor
 class SensorData:
-    def __init__(self, name, pos, radius, parent, is_clip=False):
+    def __init__(self, pos, radius, parent, is_clip=False, name=""):
         self.pos = pos
         self.radius = radius
         self.parent = parent
